1a43af140df1e5722e7ac2d0feb156543392ec9a
sabrisam <46608210+sabrisam@users.noreply.github.com>
2019-01-11 15:56:56 -0500

    Update devoir0.js

---
+++ devoir0.js
@@ -0,0 +1,15 @@
+// Équipier 1 : Sami Sabri
+
+var prenom = "Sami";
+var nom = "Sabri";
+var salutation = "Bonjour, mon nom est " + (prenom) + " " + (nom) + "!" ; 
+
+var age = 30;
+var discipline = "informatique";
+var informations = "J'ai " + (age) + " ans et j'étudie en " + (discipline) + "!";
+
+var introduction = (salutation) + "\n" + (informations);
+
+print (introduction);
+
+load("correction.js");
